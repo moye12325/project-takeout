7109c35cf3de29467fb60657cc9455a705d40a00
moye12325 <gengkang12325@gmail.com>
2024-04-05 22:08:18 +0800

    add 后端服务类

---
+++ takeout-server/src/main/java/com/moye/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.moye.service;
+
+import com.moye.dto.EmployeeLoginDTO;
+import com.moye.entity.Employee;
+
+public interface EmployeeService {
+
+    /**
+     * 员工登录
+     * @param employeeLoginDTO
+     * @return
+     */
+    Employee login(EmployeeLoginDTO employeeLoginDTO);
+
+}
