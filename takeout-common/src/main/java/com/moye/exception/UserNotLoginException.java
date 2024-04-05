af82a0339ebcfa0f0e57d0235b6676e344c87bfe
moye12325 <gengkang12325@gmail.com>
2024-04-05 22:07:12 +0800

    add 公共类

---
+++ takeout-common/src/main/java/com/moye/exception/UserNotLoginException.java
@@ -0,0 +1,12 @@
+package com.moye.exception;
+
+public class UserNotLoginException extends BaseException {
+
+    public UserNotLoginException() {
+    }
+
+    public UserNotLoginException(String msg) {
+        super(msg);
+    }
+
+}
